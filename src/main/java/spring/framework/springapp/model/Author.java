ce7de9edbbc5ba4fb151d0c782f6a3ad0fb90c21
yagosenhorini <yagosenhorini30@gmail.com>
2017-11-10 17:03:47 -0200

    Id annotation inserted on id variable

--- src/main/java/spring/framework/springapp/model/Author.java
+++ src/main/java/spring/framework/springapp/model/Author.java
@@ -2,11 +2,17 @@ package spring.framework.springapp.model;
 
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.HashSet;
 import java.util.Set;
 
 @Entity
 public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
     private String firstName;
     private String lastName;
@@ -20,6 +26,20 @@ public class Author {
     public Author(String firstName, String lastName){
         this.firstName = firstName;
         this.lastName = lastName;
+    }
+
+    public Author(String firstName, String lastName, Set<Book> books){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.books = books;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getFirstName() {
--- src/main/java/spring/framework/springapp/model/Book.java
+++ src/main/java/spring/framework/springapp/model/Book.java
@@ -2,12 +2,18 @@ package spring.framework.springapp.model;
 
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.HashSet;
 import java.util.Set;
 
 @Entity
 public class Book {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     private String title;
     private String isbn;
     private String publisher;
@@ -22,6 +28,21 @@ public class Book {
         this.title = title;
         this.isbn = isbn;
         this.publisher = publisher;
+    }
+
+    public Book(String title, String isbn, String publisher, Set<Author> authors){
+        this.title = title;
+        this.isbn = isbn;
+        this.publisher = publisher;
+        this.authors = authors;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getTitle() {
